068cac861feebf9f53e66d4bfe8feac4f1bc1e68
joshiue <jaitonit@yahoo.co.uk>
2022-09-07 09:06:14 +0100

    update

--- exec_check.c
+++ exec_check.c
@@ -1,4 +1,38 @@
+#include "main.h"
+/**
+ * _execute - function that execute and check the prosecc
+ * @child: child id
+ * @line: buffer of line
+ * @token: pointer to array to tokenize lines
+ *
+ * Return: 0
+ */
+int _execute(pid_t child, char *line, char **token)
+{
+	int errors = 1;
 
+	if (child == -1)
+	{
+		perror("faild to cereate prosecc");
+		return (1);
+	}
+	if (child == 0)
+	{
+		token = split_line(line);
+		if (_path(token) == -1)
+		{
+			return (-1);
+		}
+		else
+			errors++;
+	}
+	else
+	{
+		wait(NULL);
+	}
+	free(token);
+	return (0);
+}
 
 /**
  * input_check - function that prints error message
